1971d1baffa03ee14dec9a23e90d46e162aad480
1112211020 <1112211020@gap.wzu.edu.tw>
2025-02-24 15:58:05 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>text</title>
+</head>
+<body>
+    <ht>我的練習 php & mysq1</h1>
+
+    <p>今天是: <?= dete("Y年m月d日") ?></p>
+    <hr>
+    <p>我的身高: <?= $h=158 ?></p>
+    <p>我的體重: <?= $w=45 ?></p>
+    <p>我的BMI值: <?= $W/($h/100*$h/100) ?></p>
+    
+</body>
+</html>
